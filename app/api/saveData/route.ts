d41f361cf26f70814849de3560ae7297ac8c5847
Samir Sadek <samirsadek2009@gmail.com>
2025-02-05 15:16:55 +0600

    error check

--- app/api/saveData/route.ts
+++ app/api/saveData/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
-const DATA_FILE_PATH = path.join(process.cwd(), "data.json");
+const DATA_FILE_PATH = path.join(process.cwd(), "/tmp/data.json");
 
 export async function POST(req: Request) {
   try {
