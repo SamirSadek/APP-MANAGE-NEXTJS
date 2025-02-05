6cf335cfc66f8a64859be92a66c75437f26483b5
Samir Sadek <samirsadek2009@gmail.com>
2025-02-05 15:26:02 +0600

    error check

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -117,14 +117,14 @@ export default function FormPage() {
     };
 
     saveFormData(updatedFormData);
-    const response = await fetch("/api/saveData", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(updatedFormData),
-      });
-      const result = await response.json();
+    // const response = await fetch("/api/saveData", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(updatedFormData),
+    //   });
+    //   const result = await response.json();
 
-      if (!response.ok) throw new Error(result.message);
+    //   if (!response.ok) throw new Error(result.message);
     clearAutosaveData();
 
     toast({
